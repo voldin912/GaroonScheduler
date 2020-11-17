5d8b2892681ed77867053e3fbb16d52a8b78313a
kishidakohei <kohei99624@gmail.com>
2020-11-17 15:11:49 +0900

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 # garoon-schedule-hoster
-雑にGaroon APIのJSONを受け付けてホストするアプリケーション
+
+雑にGaroon APIのJSONを受け付けてホストするアプリケーションです。
+参照カレンダーとして利用することを想定しています。
+
+## スケジュールの保存
+
+`POST` または `PUT` メソッドでスケジュールを保存します。
+保存できるデータは [Garoon REST API で取得した予定の一覧取得](https://developer.cybozu.io/hc/ja/articles/360000440583#step2) のレスポンスデータのみです。
+保存用の名前( `name` )をクエリパラメータにて指定します。
+
+※ 公開URLに設置する場合、暗号化通信を利用した上で、推測不可能な `name` を指定することを推奨します。
+
+```sh
+curl -d @data/example.json -X PUT 'https://example.com/?name=test'
+```
+
+## スケジュールの取得
+
+`GET` メソッドで保存したスケジュールを取得します。
+取得する名前( `name` )、拡張子( `ext` )をクエリパラメータで指定します。
+利用可能な拡張子は `ics` `json` のみです。
+拡張子 `ics` にて `url` パラメータを指定した場合、イベントに指定されたURLをベースとしたイベントURLを付与します。
+
+```sh
+curl 'https://example.com/?name=test&ext=ics&url=https://example.com/scripts/grn.exe'
+```
+
+## Query Parameter
+
+| Name | Summary | Descibe
+| :- | :- | :-
+| name | スケジュール名 | 取得するスケジュール名(ID)
+| ext | 拡張子 | 取得する拡張子 (ics/jsonのみ)
+| url | ベースURL | イベントURL生成用ベースURL
+
---
+++ index.php
@@ -0,0 +1,180 @@
+<?php
+
+mb_internal_encoding("UTF-8");
+
+function run() {
+    $dir = dirname(__FILE__) . '/data';
+
+    $method = $_SERVER['REQUEST_METHOD'];
+    $name = isset($_GET['name']) ? $_GET['name'] : '';
+    $ext = isset($_GET['ext']) ? $_GET['ext'] : 'json';
+    $url = isset($_GET['url']) ? rtrim($_GET['url'], '/') : '';
+
+    if ($name === '') {
+        http_response_code(400);
+        die('name required.');
+    }
+
+    if ($method === 'POST' || $method === 'PUT') {
+        $data = file_get_contents('php://input');
+        if (json_decode($data) === null || !isset($data['events'])) {
+            http_response_code(400);
+            die('invalid json data');
+        }
+        file_put_contents($dir . '/' . $name . '.json', $data);
+        http_response_code(204);
+        exit;
+    }
+
+    if ($method !== 'GET') {
+        http_response_code(400);
+        die('invalid method');
+    }
+
+    $file = $dir . '/' . $name . '.json';
+    if (!file_exists($file)) {
+        http_response_code(404);
+        die('not found');
+    }
+
+    $json = file_get_contents($file);
+    if ($json === null ) {
+        http_response_code(500);
+        die('invalid data');
+    }
+
+    if ($ext === 'json') {
+        header('Content-Type: application/json');
+        echo $json;
+        exit;
+    }
+
+    if ($ext !== 'ics' && $ext !== 'txt') {
+        http_response_code(400);
+        die('invalid ext');
+    }
+
+    $data = json_decode($json, true);
+
+    if ($ext === 'txt') {
+        header('Content-Type: text/plain');
+    } else {
+        header('Content-Type: text/calendar');
+    }
+    echo toICal($data, $url);
+}
+
+function toICal($json, $baseURL = null) {
+    $data = array_filter(array_merge(
+        [
+            'BEGIN:VCALENDAR',
+            'CALSCALE:GREGORIAN',
+            'PRODID:-//kamiaka//garoon-scheudle-hoster v1.0//JP',
+            'METHOD:PUBLISH',
+            'VERSION:2.0',
+            'X-WR-CALNAME:Garoon Schedule',
+            'X-WR-CALDESC:Hosted Garoon Schedule',
+            'X-WR-TIMEZONE:Asia/Tokyo',
+            'BEGIN:VTIMEZONE',
+            'TZID:Asia/Tokyo',
+            'BEGIN:STANDARD',
+            'DTSTART:19390101T000000',
+            'TZOFFSETFROM:+0900',
+            'TZOFFSETTO:+0900',
+            'TZNAME:JST',
+            'END:STANDARD',
+            'END:VTIMEZONE',
+        ],
+        toICalEvents($json['events'] ?: [], $baseURL),
+        [
+            'END:VCALENDAR',
+        ],
+    ));
+    return implode("\r\n", $data) . "\r\n";
+}
+
+function toICalEvents($events, $baseURL = null) {
+    $data = [];
+    $maxAttendees = 20;
+    foreach ($events as $ev) {
+        $data = array_merge($data, [
+            'BEGIN:VEVENT',
+            field('UID', uid($ev['id'], $baseURL) . '/' . $ev['start']['dateTime']),
+            field("ORGANIZER;CN={$ev['creator']['name']}", $ev['creator']['code']),
+            toICalTimeField('CREATED', $ev['createdAt'], 'UTC'),
+            toICalTimeField('LAST-MODIFIED', $ev['updatedAt'], 'UTC'),
+            toICalTimeField('DTSTART', $ev['start']['dateTime'], $ev['start']['timeZone']),
+            toICalTimeField('DTEND', $ev['end']['dateTime'], $ev['end']['timeZone']),
+            field('CLASS', 'PRIVATE'),
+            field('SUMMARY', ($ev['eventMenu'] ? "{$ev['eventMenu']}: " : '') . $ev['subject']),
+            field('DESCRIPTION', implode("\n", array_filter([
+                trim($ev['notes']),
+                '- - -',
+                "Created by: {$ev['creator']['name']} ({$ev['createdAt']})",
+                "Updated by: {$ev['updater']['name']} ({$ev['updatedAt']})",
+                "Attendees: " . implode(', ', array_map(function($item) {
+                    return $item['name'];
+                }, array_slice($ev['attendees'], 0, $maxAttendees))) . (count($ev['attendees']) > $maxAttendees ? (', ...more ' . (count($ev['attendees']) > $maxAttendees) . ' attendees') : '')
+            ]))),
+            field('LOCATION', empty($ev['facilities']) ? '' : implode(', ', array_map(function($item) {
+                return $item['name'];
+            }, $ev['facilities']))),
+            field('URL', generateEventURL($ev['id'], $baseURL)),
+            'END:VEVENT',
+        ]);
+    }
+    return $data;
+}
+
+function toICalTimeField($field, $timeString, $zoneString = null) {
+    $time = new DateTime($timeString);
+    $zone = new DateTimeZone($zoneString ?: 'Asia/Tokyo');
+    $time->setTimezone($zone);
+    if ($zoneString === 'UTC') {
+        $time->setTimezone($zone);
+        return sprintf('%s:%s', $field, $time->format('Ymd\THis\Z'));
+    }
+    return sprintf('%s;TZID=%s:%s', $field, $zone->getName() ?: $time->getTimezone()->getName(), $time->format('Ymd\THis'));
+}
+
+function field($name, $value) {
+    if (empty($value)) {
+        return '';
+    }
+
+    $field = $name . ':' . $value;
+    $field = str_replace(["\r", "\n"], ['\r', '\n'], $field);
+
+    return splitField($field, 72);
+}
+
+function splitField($field, $length, $indent = "\t", $break = "\n") {
+    $chars = mb_str_split($field);
+    $bytes = 0;
+    $str = '';
+    foreach ($chars as $char) {
+        $bytes += strlen($char);
+        if ($bytes > $length) {
+            $bytes = strlen($indent . $char);
+            $str .= $break . $indent;
+        }
+        $str .= $char;
+    }
+    return $str;
+}
+
+function generateEventURL($id, $baseURL = null) {
+    if (empty($baseURL)) {
+        return '';
+    }
+    return $baseURL . '/schedule/view?event=' . $id;
+}
+
+function uid($id, $url = null) {
+    if ($url) {
+        return $id . '@' . preg_replace('@https?://([^/]+)/?.*@', '$1', $url);
+    }
+    return $id . '@'. $_SERVER['SERVER_NAME'];
+}
+
+run();
